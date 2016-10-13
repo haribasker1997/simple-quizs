2d58da32b721d34e5ee232c9be32a229900ed1c9
rajnish7t <rajnish7t@gmail.com>
2016-10-13 16:02:54 +0530

    simplequiz
    html, js, css

---
+++ quizjs.js
@@ -0,0 +1,4 @@
+function getAnswer(input) {
+if (input == "a") {alert ("Well done! The first Table Tennis Patent was issued in England on the 15th of July 1890.")}
+else {alert ("No that's incorrect, please try again...")}
+}
